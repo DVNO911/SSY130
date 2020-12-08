55bf077c909ac6fdc14ad2adb3e6fd7108f23ead
albben97 <bealbin@student.chalmers.se>
2020-12-08 15:00:41 +0100

    C finished

--- dspkit/src/lab_lms.c
+++ dspkit/src/lab_lms.c
@@ -434,14 +434,15 @@ void my_lms(float const * y, float const * x, float * xhat, float * e, int block
 	 *   lms_coeffs += 2 * mu * y_book * e[n];      //Use some type of loop to update the vector lms_coeffs with the vector y multiplied by scalars 2, mu, e[n].
 	 * }
 	 * ...to here */
-	int n;
-	for(n = 0; n < block_size; n++) {
+	int n, m, i;
+	for(n = 0; n<block_size; n++){
 		float * y_book = &lms_state[n];
-		//float * xhat_n = &xhat[n];
-		arm_dot_prod_f32(lms_coeffs ,y_book, block_size, &xhat[n]);
-		e[n] = x[n]-xhat[n];
-		for(int m = n; m < block_size; m++) {
-			lms_coeffs[n] += 2*lms_mu*y_book[m]*e[n];
+		arm_dot_prod_f32(lms_coeffs, y_book, lms_taps, xhat+n);
+		e[n] = x[n] - xhat[n];
+
+
+		for(i = 0; i<=lms_taps; i++){
+			lms_coeffs[i] += 2* lms_mu * y_book[i] * e[n];
 		}
 	}
 #endif
